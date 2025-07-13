5d32fddf0f754dfaf917978112f6c63bedb93c31
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-07-13 00:58:02 -0400

    Added option to change tasks status

--- src/components/tasks/TaskForm.jsx
+++ src/components/tasks/TaskForm.jsx
@@ -6,23 +6,26 @@ const API_URL = process.env.REACT_APP_API_URL;
 const TaskForm = ({currentTask, onTaskUpdated}) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [status, setStatus] = useState("Pendiente");
 
     useEffect(() => {
         if (currentTask) {
             setTitle(currentTask.titulo)
             setDescription(currentTask.descripcion)
+            setStatus(currentTask.estado)
         }
     }, [currentTask]);
 
     const handleSubmit = async() => {
         if (currentTask) {
-            await axios.put(`${API_URL}/tasks/${currentTask._id}`, {titulo: title, descripcion: description})
+            await axios.put(`${API_URL}/tasks/${currentTask._id}`, {titulo: title, descripcion: description, estado: status})
         } else {
             await axios.post(`${API_URL}/tasks`, {titulo: title, descripcion: description});
         }
         onTaskUpdated();
         setTitle('');
         setDescription('');
+        setStatus("Pendiente");
     }
 
     return (
@@ -39,6 +42,28 @@ const TaskForm = ({currentTask, onTaskUpdated}) => {
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
             />
+            {(currentTask && 
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Pendiente"
+                            checked={status=="Pendiente"}
+                            onChange={(e) => setStatus(e.target.value)}
+                        />
+                        Pendiente
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="Completado"
+                            checked={status=="Completado"}
+                            onChange={(e) => setStatus(e.target.value)}
+                        />
+                        Completada
+                    </label>
+                </div>
+            )}
             <button type="submit">{currentTask ? 'Actualizar' : 'Agregar'} Tarea</button>
         </form>
     );
