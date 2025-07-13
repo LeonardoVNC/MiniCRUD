6ff4ba1ac0a1e13df2277029b3ab7a6ceee4be28
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-07-13 00:21:15 -0400

    Adding styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import React, { useState } from 'react';
+import './styles.css';
+import { useState } from 'react';
 import axios from 'axios';
 import TaskForm from './components/tasks/TaskForm';
 import TaskList from './components/tasks/TaskList';
